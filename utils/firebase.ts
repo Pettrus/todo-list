62b42bf8297cecad07e7b382767fbfe04e08bed2
Pettrus <pettrus.sherlock@gmail.com>
2022-03-31 21:30:03 +0000

    Initial commit

---
+++ utils/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVw1yV_yQdfsgX6K1Ybr0bVvfsMdxcvLQ",
+    authDomain: "planless-2d062.firebaseapp.com",
+    databaseURL: "https://planless-2d062-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "planless-2d062",
+    storageBucket: "planless-2d062.appspot.com",
+    messagingSenderId: "178008591722",
+    appId: "1:178008591722:web:738ee49834c9e61abb6913"
+};
+
+const app = initializeApp(firebaseConfig);
